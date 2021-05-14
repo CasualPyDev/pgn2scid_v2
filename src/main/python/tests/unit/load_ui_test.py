f287e09fc5edc5fac473f83e06111180be7f3b31
Andreas Kreisig <andreaskreisig@gmail.com>
2021-05-14 18:06:38 +0200

    main.ui moved, changed test accordingly

--- src/main/python/tests/unit/load_ui_test.py
+++ src/main/python/tests/unit/load_ui_test.py
@@ -5,6 +5,6 @@ from main import Ui
 class LoadUiTest(TestCase):
     def test_load_ui(self):
         testval = True
-        with open('../../ui/main.ui', 'r') as f:
+        with open('../../main.ui', 'r') as f:
             data = f.read()
         self.assertTrue(data, testval)
