2631c8eab447fe80dc439c05dba0cb52a7667616
Andreas Kreisig <andreaskreisig@gmail.com>
2022-12-26 19:17:14 +0100

    Set correct version number in toml file

--- tests/test_pgn2scid_v2.py
+++ tests/test_pgn2scid_v2.py
@@ -1,7 +1,8 @@
 # tests/test_pgn2scid.py
 
 from importlib.metadata import version
-v = version('pgn2scid-v2')
+
+v = version('pgn2scid_v2')
 
 def test_version():
-    assert v == "0.1.0"
+    assert v == "2.1.0"
