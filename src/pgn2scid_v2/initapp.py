d184b55c7376a5803fbe836ef89012e72fc776a2
Andreas Kreisig <andreaskreisig@gmail.com>
2022-12-27 11:43:37 +0100

    Basic exception handling added

--- src/pgn2scid_v2/initapp.py
+++ src/pgn2scid_v2/initapp.py
@@ -1,9 +1,14 @@
 import tomli
 
+def create_toml_file():
+    pass
+
 def read_config():
+    try:
         with open ('pgn2scid.toml', mode='rb') as fp:
             config = tomli.load(fp)
         return config
-
+    except FileNotFoundError:
+        create_toml_file()
 
 app_config_read = read_config()
