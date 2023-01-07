fc209377f7876c18fa7368470c0737b928d65637
Andreas Kreisig <andreaskreisig@gmail.com>
2023-01-07 20:48:43 +0100

    Unit test for initapp.py added

--- tests/test_initapp.py
+++ tests/test_initapp.py
@@ -8,7 +8,7 @@ import time
 from pgn2scid_v2 import initapp
 
 toml_config_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src', 'pgn2scid_v2'))
-toml_config_file = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src', 'pgn2scid_v2', 'pgn2scid.toml'))
+toml_config_file = os.path.abspath(os.path.join(toml_config_path, 'pgn2scid.toml'))
 
 
 def test_version():
